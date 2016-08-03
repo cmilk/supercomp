0bb248d4e5cc2860c8783a8ad19588524db4415c
yeltsov <yeltsov.ilya@gmail.com>
2016-08-04 01:10:35 +0500

    typography and fixes

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,12 @@ var countTimer1 = new CountUp("firstService", countStart, 100, 0, 2);
 var countTimer2 = new CountUp("secondService", countStart, 149, 0, 2);
 var countTimer3 = new CountUp("thirdService", countStart, 169, 0, 2);
 var timerPoint = $('.statistics').waypoint({
-    handler: function(direction) {
+    handler: function() {
         countTimer1.start();
         countTimer2.start();
         countTimer3.start();
     },
-    offset: '60%'
+    offset: '50%'
 });
 
 var nav = $('#nav');
--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,28 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const ProvidePlugin = webpack.ProvidePlugin;
 
 module.exports = {
-  resolve: {
-      alias: {
-          "waypoints": path.resolve(
-              __dirname,
-              "node_modules/waypoints/lib/jquery.waypoints.min"
-          ),
-      }
-  },
-  plugins: [
-    new ProvidePlugin({
-      $: "jquery/dist/jquery.slim.min.js",
-      jQuery: "jquery/dist/jquery.slim.min.js",
-      "window.jQuery": "jquery/dist/jquery.slim.min.js"
-    }),
-    new UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },  
-      sourceMap: false,
-      mangle: false
-    })
-  ],
+    resolve: {
+        alias: {
+            "waypoints": path.resolve(
+                __dirname,
+                "node_modules/waypoints/lib/jquery.waypoints.min"
+            ),
+        }
+    },
+    plugins: [
+        new ProvidePlugin({
+            $: "jquery/dist/jquery.slim.min.js",
+            jQuery: "jquery/dist/jquery.slim.min.js",
+            "window.jQuery": "jquery/dist/jquery.slim.min.js"
+        }),
+        new UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: false,
+            mangle: false
+        })
+    ],
     entry: "./src/js/main.js",
     output: {
         path: __dirname + "/dist/js",
