547f62d5c99aa46a6db6de53958b8f5eea177392
yeltsov <yeltsov.ilya@gmail.com>
2016-08-03 23:47:01 +0500

    webpack complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,25 @@
-(function($) {
-	var countStart = 999;
-	var countTimer1 = new CountUp("firstService", countStart, 100, 0, 2);
-	var countTimer2 = new CountUp("secondService", countStart, 149, 0, 2);
-	var countTimer3 = new CountUp("thirdService", countStart, 169, 0, 2);
-	var timerPoint = $('.statistics').waypoint({
-	  handler: function(direction) {
-	    countTimer1.start();
-			countTimer2.start();
-			countTimer3.start();
-	  }, offset: '60%'
-	});
+var CountUp = require("countup.js");
+var waypoint = require("waypoints");
 
-	var nav = $('#nav');
-	var pos = nav.position().top;
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > pos) {
-					nav.addClass("alt");
-			} else {
-					nav.removeClass("alt");
-			}
-	});
-})(jQuery);
+var countStart = 999;
+var countTimer1 = new CountUp("firstService", countStart, 100, 0, 2);
+var countTimer2 = new CountUp("secondService", countStart, 149, 0, 2);
+var countTimer3 = new CountUp("thirdService", countStart, 169, 0, 2);
+var timerPoint = $('.statistics').waypoint({
+    handler: function(direction) {
+        countTimer1.start();
+        countTimer2.start();
+        countTimer3.start();
+    },
+    offset: '60%'
+});
+
+var nav = $('#nav');
+var pos = nav.position().top;
+$(window).scroll(function() {
+    if ($(this).scrollTop() > pos) {
+        nav.addClass("alt");
+    } else {
+        nav.removeClass("alt");
+    }
+});
